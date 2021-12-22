02579bab7e1b84fd4a2171a762fb17d114bb28f0
haseeb0118 <haseeburrahman47@gmail.com>
2021-12-22 11:04:23 +0500

    changes_rough

---
+++ lib/realtime_database/rough_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoughPage extends StatefulWidget {
+  const RoughPage({Key? key}) : super(key: key);
+
+  @override
+  _RoughPageState createState() => _RoughPageState();
+}
+
+class _RoughPageState extends State<RoughPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
